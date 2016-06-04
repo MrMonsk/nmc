3254e563ba9315b554c1d370d248d6fca910c0de
Yowon Yoon <yowonyoon@gmail.com>
2016-06-04 14:18:39 +0000

    moved new work into a card

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-<<<<<<< HEAD
   resources :works, :only => [:index, :show, :new, :create]
-=======
-  resources :works, only: [:index, :show]
->>>>>>> e36f6dfe2ccfe0f092cf330307432b0a1eb368af
 end
