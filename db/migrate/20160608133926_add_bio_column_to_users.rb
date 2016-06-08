ccbee58c0e24ec193c2d3c7ad8d9411951622ecb
Yowon Yoon <yowonyoon@gmail.com>
2016-06-08 14:18:02 +0000

    removed migration, going to create separate model to store user profile info

--- db/migrate/20160608133926_add_bio_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBioColumnToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :bio, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608133926) do
+ActiveRecord::Schema.define(version: 20160606054028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20160608133926) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
