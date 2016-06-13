b6a7e6539e515f6852eae4020932ef8371795e8c
Yowon Yoon <yowonyoon@gmail.com>
2016-06-13 14:54:28 +0000

    fixed tests baby

--- spec/controllers/performances_controller_spec.rb
+++ spec/controllers/performances_controller_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe PerformancesController, type: :controller do
 
   describe 'GET #show' do
     it 'returns http success' do
-      get :show
+      user = FactoryGirl.create(:user)
+      sign_in user
+      get :show, id: 1
       expect(response).to have_http_status(:success)
     end
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :email do |n|
+      "test_user#{n}@gmail.com"
+    end
+    password "volleyballisagirlssport"
+    password_confirmation "volleyballisagirlssport"
+  end
+end
