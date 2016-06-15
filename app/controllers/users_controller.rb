57ce8f1b2e67bdc8474d10cb6b2ae1d241435130
Yowon Yoon <yowonyoon@gmail.com>
2016-06-15 18:33:27 +0000

    different views depending on whether the signed in user matches user_id of show, wrote tests as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @works = @user.works
+    begin
+      @user = User.find(params[:id])
+      @works = @user.works
+    rescue ActiveRecord::RecordNotFound
+        render text: 'user not found :/', status: :not_found
+    end
   end
   
   def edit
