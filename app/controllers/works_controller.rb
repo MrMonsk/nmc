981b07040a1e6d42117aba6f7bddcf2ad74692a2
Yowon Yoon <yowonyoon@gmail.com>
2016-06-17 16:16:22 +0000

    fixed update in works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -22,12 +22,19 @@ class WorksController < ApplicationController
     @work = Work.find_by_id(params[:id])
     if @work.blank?
       render text: 'Work not found :/', status: :not_found
+    elsif @work.user != current_user
+      render text: 'You are not the composer of this work!', status: :forbidden
     end
   end
   
   def update
     @work = Work.find_by_id(params[:id])
-    redirect_to user_path(id: current_user.id), notice: 'work updated successfully'
+    if current_user == @work.user 
+      @work.update_attributes(work_params)
+      redirect_to user_path(id: current_user.id), notice: 'work updated successfully'
+    else
+      render text: 'Oopsie', status: :forbidden
+    end
   end
 
   private
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,3 @@
 class Work < ActiveRecord::Base
   belongs_to :user
-  accepts_nested_attributes_for :user
-  mount_uploader :work, WorkUploader
 end
