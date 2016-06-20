c9536deda155f9f3ff22dfc8fc518c57db7f268c
Yowon Yoon <yowonyoon@gmail.com>
2016-06-20 16:37:49 +0000

    following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,25 @@ class User < ActiveRecord::Base
   has_many :works
   has_many :performances
   has_one :profile
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def follow(user_to_follow)
+    active_relationships.create(followed_id: user_to_follow.id)
+  end
+  
+  def unfollow(user_to_unfollow)
+    active_relationsihps.find_by(followed_id: user_to_unfollow.id).destroy
+  end
+  
+  def following?(some_user)
+    following.include?(some_user)
+  end
 end
