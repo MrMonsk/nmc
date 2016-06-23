3607219ec11379d742068d5e9c73adba11069817
Stewart Sanchez <slsgz4@mst.edu>
2016-06-23 16:44:40 +0000

    Added a new performance button from the user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @works = @user.works
+    @performances = @user.performances
   rescue ActiveRecord::RecordNotFound
     render text: 'user not found :/', status: :not_found
   end
